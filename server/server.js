67b2be0f50bd7977e0f602fc04ff859c4d3ed29a
Shervin Kayvon <shervinkayvon@gmail.com>
2018-12-08 15:12:37 +0100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(3000, () => {
+    console.log(`Started on port ${port}`);
+});
