be724bacdf2456d5c85041fe2957bb9af3bb7e05
Shervin Kayvon <shervinkayvon@gmail.com>
2018-12-09 14:41:07 +0100

    Scroll to bottom if user is close to bottom

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -19,6 +33,7 @@ socket.on('newMessage', function (message) {
     });
     
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -31,6 +46,7 @@ socket.on('newLocationMessage', function (message) {
     });
     
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
